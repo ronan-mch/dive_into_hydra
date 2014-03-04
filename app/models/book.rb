ef6a6a6ebc79eb86d808f09af494c9c6f41c2036
Ronan McHugh <mchugh.r@gmail.com>
2014-03-04 10:32:49 +0100

    created book model and datastream

---
+++ app/models/book.rb
@@ -0,0 +1,6 @@
+class Book < ActiveFedora::Base
+  has_metadata 'descMetadata', type: BookMetadata
+
+  has_attributes :title, datastream: 'descMetadata', multiple: false
+  has_attributes :author, datastream: 'descMetadata', multiple: false
+end
