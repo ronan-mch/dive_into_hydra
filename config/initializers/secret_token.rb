533980cb978cf7b83b78a54f9b91620863f689ff
Ronan McHugh <mchugh.r@gmail.com>
2014-03-04 09:13:33 +0100

    initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DiveIntoHydra::Application.config.secret_key_base = 'bbbb58634b508fae1e382f66401d22696055e05fb464ab71d31ac5647d51ffbb9483e423cc326b1f543e47c7c81797e97a6d86eea1e81e11b1cc36b471977da7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DiveIntoHydra::Application.config.session_store :cookie_store, key: '_dive_into_hydra_session'
